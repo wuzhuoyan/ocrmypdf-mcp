7006e9958e72e1983c259000c77989731a893da5
wuzhuoyan <wuzhuoyan@live.com>
2025-06-13 14:42:49 +0800

    update

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ app.include_router(ocr_pdf_router)
 app.include_router(health_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=9090, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9090, reload=True)
